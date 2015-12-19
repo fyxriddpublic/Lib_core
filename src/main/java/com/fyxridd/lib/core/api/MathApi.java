dcd6560a7273df8aa45c2dcac88ac0e6beadc8b6
fyxridd <fyxridd@163.com>
2015-12-19 13:02:50 +0800

    更新

--- src/main/java/com/fyxridd/lib/core/api/MathApi.java
+++ src/main/java/com/fyxridd/lib/core/api/MathApi.java
@@ -5,11 +5,19 @@ import org.apache.commons.math3.random.RandomDataGenerator;
 public class MathApi {
     public static final org.apache.commons.math3.random.RandomDataGenerator RandomDataGenerator = new RandomDataGenerator();
 
+    /**
+     * upper >= lower
+     */
     public static int nextInt(int lower, int upper) {
+        if (upper == lower) return upper;
         return RandomDataGenerator.nextInt(lower, upper);
     }
 
+    /**
+     * upper >= lower
+     */
     public static long nextLong(long lower, long upper) {
+        if (upper == lower) return upper;
         return RandomDataGenerator.nextLong(lower, upper);
     }
 }
