2e79f45f44c93e13045f2464b1274dc172807104
fyxridd <fyxridd@163.com>
2015-12-19 12:06:26 +0800

    更新

---
+++ src/main/java/com/fyxridd/lib/core/api/getter/MultiRandomLong.java
@@ -0,0 +1,37 @@
+package com.fyxridd.lib.core.api.getter;
+
+import com.fyxridd.lib.core.api.hashList.ChanceHashList;
+import com.fyxridd.lib.core.api.hashList.ChanceHashListImpl;
+
+/**
+ * 随机长整数分段获取器
+ */
+public class MultiRandomLong implements RandomGetter{
+    private ChanceHashList<RandomLong> chances = new ChanceHashListImpl<>();
+
+    /**
+     * @param data '<概率>:<RandomLong>[,MultiRandomLong]',概率>=0
+     */
+    public MultiRandomLong(String data) {
+        String[] args = data.split(",", 2);
+        //chances
+        {
+            String[] args2 = args[0].split(":");
+            chances.addChance(new RandomLong(args2[1]), Integer.parseInt(args2[0]));
+        }
+        //next
+        if (args.length > 1) {
+            MultiRandomLong next = new MultiRandomLong(args[1]);
+            chances.convert(next.chances, false);
+        }
+    }
+
+    /**
+     * @param extra 此处无法加成
+     * @return >=0的整数
+     */
+    @Override
+    public Object get(int extra) {
+        return chances.getRandom().get(extra);
+    }
+}
---
+++ src/main/java/com/fyxridd/lib/core/api/getter/RandomLong.java
@@ -0,0 +1,43 @@
+package com.fyxridd.lib.core.api.getter;
+
+import com.fyxridd.lib.core.api.MathApi;
+
+/**
+ * 随机长整数获取器
+ * 可以是任意长整数,abs(max) < Long最大值/100,abs(min) < Long最大值/100
+ */
+public class RandomLong implements RandomGetter{
+    private long min, max;
+
+    /**
+     * @param data 'min长整数->max长整数'或'长整数',其中max>=min
+     */
+    public RandomLong(String data) {
+        if (data.contains("-")) {
+            String[] args = data.split("\\->");
+            min = Long.parseLong(args[0]);
+            max = Long.parseLong(args[1]);
+        }else {
+            min = Long.parseLong(data);
+            max = min;
+        }
+    }
+
+    /**
+     * @param extra 此处可以加成
+     * @return >=0的整数
+     */
+    @Override
+    public Object get(int extra) {
+        long result = MathApi.nextLong(min, max);
+        if (extra > 0) {
+            //检测加成
+            long tmp = result*(100+extra);
+            result = tmp/100;
+            if (MathApi.nextInt(0, 99) < (int)(tmp%100)) result++;
+            //检测上限
+            result = Math.min(result, max);
+        }
+        return result;
+    }
+}
